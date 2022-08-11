7a858133e8829e0fa13b44b4e4b5ec1c66e3f216
RLazamh <roger.laza@epn.edu.ec>
2022-08-11 15:21:40 -0500

    add ParseUIID concept

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -17,7 +17,7 @@ export class CarsController {
 
   @Get(':id')
   getCarById(
-    @Param('id' , ParseIntPipe) id : number  
+    @Param('id' , ParseUUIDPipe) id : string  
   ){
     return this._carsService.findById( id );
   }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,21 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 
+import { Car } from './interfaces/car.interface';
 @Injectable() //este decorador permite que la clase sea inyectable 
 export class CarsService {
 
-    private cars = [
+    private cars : Car[] = [
         {
-            id    : 1,
+            id    : uuid(),
             brand :'ferrari',
             model : 'Corolla'
         }, 
         {
-            id    : 2,
+            id    : uuid(),
             brand :'Honda',
             model : 'Civic'
         }, 
         {
-            id    : 3,
+            id    : uuid(),
             brand :'Jeep',
             model : 'Cheroken'
         },
@@ -25,7 +27,7 @@ export class CarsService {
         return this.cars;
     }
 
-    findById( id : number ) {
+    findById( id : string ) {
         const car =  this.cars.find(car => car.id === id );
 
         if(!car)  throw new NotFoundException(` Car with id ${id} not found`);
